03a16d7eb50fecd6d62663e8af8ae16c37ca154f
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-05-26 15:05:36 -0500

    Initial Commit

---
+++ backend/src/main/java/com/SSDetailing/SsDetailingApplication.java
@@ -0,0 +1,13 @@
+package com.SSDetailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsDetailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsDetailingApplication.class, args);
+	}
+
+}
