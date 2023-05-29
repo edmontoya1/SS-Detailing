494c6817cbb3e081e59b2f6f052b3eb3a06bea01
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-05-29 12:43:15 -0500

    Initial Appointment Controller commit

---
+++ backend/src/main/java/com/SSDetailing/controller/AppointmentController.java
@@ -0,0 +1,17 @@
+package com.SSDetailing.controller;
+
+import com.SSDetailing.service.AppointmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/appointments")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
+}
